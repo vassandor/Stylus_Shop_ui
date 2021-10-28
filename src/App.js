c93a8c71fa709a75ee45189f7f941dc62c3afc77
Sandor Vas <snvas15@gmail.com>
2021-10-28 20:52:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import Menu from "./components/Menu";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-
 function App() {
   return (
     <div>
-        <Menu/>
 
-        <Content/>
-
-        <Footer
-            title="Stylus Shop"
-            address="Budapest"
-            phone="+36-30-123-4567"
-            email="stylusshop@gmail.com"
-        />
     </div>
   );
 }
